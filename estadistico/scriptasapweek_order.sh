84cdf3881d5cdff9e38a64ea841e6aca5ea81530
Juan Pablo <juanpablo.cares@gmail.com>
2014-03-27 10:36:38 -0400

    arreglado el asap y agregado el asap por semana ordenado

---
+++ estadistico/scriptasapweek_order.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MainDir=/home/jp/Documents/tesis/radiotherapy/radiotherapy/results/asapweek_order
+OutDir=/home/jp/Documents/tesis/radiotherapy/radiotherapy/estadistico/output
+
+cd $MainDir
+
+ls | \
+    (while read i;
+    do 
+out=$OutDir"/"$i
+    mkdir $out;
+    done)
+
+find -name "*.out" -print0 | \
+    (while read -d $'\0' i;
+    do
+out=$OutDir"/"${i/out/est};
+    ./../../estadistico/main < ${i:2} > ${out};
+    done)
